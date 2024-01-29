ee4af0dc93a117f9f6e936434e1a3dc41c6ca5d5
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-01-28 21:49:36 -0600

    practice: add best time to read me

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 2024-01-28-client
 
 A client for coding practice.
+
+✔ Best Time 5:22. 
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function addButton() {
+function addButton () {
     const button = document.createElement("button");
     button.innerHTML = "Click Me!";
     button.onclick = () => {
-        console.log("Button clicked!");
-    }
+        console.log("Button Clicked!");
+    };
     const div = document.getElementById("addButton");
     div.appendChild(button);
 }
